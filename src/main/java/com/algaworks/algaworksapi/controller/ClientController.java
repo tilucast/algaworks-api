ef15dd38bd81e2567ba063d0ebe4c08b21e618b3
Lucas <tilucast@gmail.com>
2021-08-12 18:10:24 -0300

    spring data jpa added and configured

--- src/main/java/com/algaworks/algaworksapi/controller/ClientController.java
+++ src/main/java/com/algaworks/algaworksapi/controller/ClientController.java
@@ -5,28 +5,31 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algaworksapi.domain.model.Client;
+import com.algaworks.algaworksapi.domain.repository.ClientRepository;
+
+import lombok.AllArgsConstructor;
 
 @RestController
+@AllArgsConstructor
 public class ClientController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	//@Autowired
+	private ClientRepository clientRepository;
 
 	@GetMapping("/")
 	public String get() {
-		System.out.println("I am working dude");
 		return "Hey dude";
 	}
 	
 	@GetMapping("/clients")
 	public List<Client> getClients() {
 		
-		return manager.createQuery("from Client", Client.class)
-				.getResultList();
+		return clientRepository.findAll();
 	}
 	
 }
