9f672cc3f7608cbbb65a38aef188dcefc7a3789e
Lucas <tilucast@gmail.com>
2021-08-12 21:20:17 -0300

    client CRUD implemented

--- src/main/java/com/algaworks/algaworksapi/controller/ClientController.java
+++ src/main/java/com/algaworks/algaworksapi/controller/ClientController.java
@@ -2,11 +2,16 @@ package com.algaworks.algaworksapi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algaworksapi.domain.model.Client;
@@ -16,6 +21,7 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/clients")
 public class ClientController {
 	
 	//@Autowired
@@ -26,10 +32,57 @@ public class ClientController {
 		return "Hey dude";
 	}
 	
-	@GetMapping("/clients")
+	@GetMapping
 	public List<Client> getClients() {
 		
 		return clientRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Client> getById(@PathVariable Long id) {
+		
+		return clientRepository.findById(id)
+				//.map(client -> ResponseEntity.ok(client))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		
+//		Optional<Client> client = clientRepository.findById(id);
+//		
+//		if(client.isPresent()) {
+//			return ResponseEntity.ok(client.get());
+//		}
+//		
+//		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client create(@RequestBody Client client) {
+		return clientRepository.save(client);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Client> update(@PathVariable Long id, @RequestBody Client client) {
+		
+		if(!clientRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		client.setId(id);
+		client =  clientRepository.save(client);
+		
+		return ResponseEntity.ok(client);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id){
+		if(!clientRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clientRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
